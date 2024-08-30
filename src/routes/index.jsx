0d59354485fa72c528456c9594e42703f09f268e
larinhab <larabviana@outlook.com>
2024-08-30 10:47:20 -0300

    :bug: fix import router-dom

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,17 +2,18 @@ import { Home } from "../pages/Home"
 import { About } from '../pages/About'
 import { Contact } from '../pages/Contact'
 import { Projects } from '../pages/Projects'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom"
 
 export function AppRoutes(){ 
     return(
-        <Router>
-             <RouterRoutes>
+        <BrowserRouter>
+             <Routes>
              <Route path="/" element={<Home />} />
              <Route path="/about" element={<About />} />
              <Route path="/contact" element={<Contact />} />
              <Route path="/projects" element={<Projects />} />
-        </RouterRoutes>
-        </Router>
+        </Routes>
+        </BrowserRouter>
     )
 }
